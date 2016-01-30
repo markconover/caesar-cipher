173c7bab893468442ed6fa3c5dd84617d4adc701
Mark Conover <conoverm157@gmail.com>
2016-01-29 21:55:09 -0500

    Committing initial project structure.

---
+++ caesar-cipher-jar/src/main/java/mark/conover/crypto/CaesarCipherMain.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package mark.conover.crypto;
+
+/**
+ * @author Mark Conover (conoverm157@gmail.com)
+ */
+public class CaesarCipherMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
